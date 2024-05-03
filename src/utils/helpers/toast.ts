f37704b80cf61ab60b41aa83387d1fd53365e7ee
Kent Leow <zqleow@palo-it.com>
2024-05-03 16:16:42 +0800

    fix(framework): fix error toast when message is in array

--- src/utils/helpers/toast.ts
+++ src/utils/helpers/toast.ts
@@ -10,10 +10,13 @@ const toastSettings: ToastOptions = {
 };
 
 export const displayErrorToast = (e: unknown) => {
-  if (isAxiosError(e))
-    toast(e.response?.data.message, { type: 'error', ...toastSettings });
-  else if (e instanceof Error)
+  if (isAxiosError(e)) {
+    const error = e.response?.data.message;
+    const errorMessage = Array.isArray(error) ? error.join(', ') : error;
+    toast(errorMessage, { type: 'error', ...toastSettings });
+  } else if (e instanceof Error) {
     toast(e.message, { type: 'error', ...toastSettings });
+  }
 };
 
 export const displaySuccessToast = (message: string) => {
