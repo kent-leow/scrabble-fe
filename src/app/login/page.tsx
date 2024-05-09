587fd8f187bf22023284068cce3f61e3070a6f91
Kent Leow <zqleow@palo-it.com>
2024-05-09 12:59:08 +0800

    chore(log): remove console log

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,7 +22,6 @@ export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
   const { register, handleSubmit } = useForm<ILoginForm>();
   const { login } = useContext(AuthContext);
-  console.log('LoginPage');
   const handleClickShowPassword = () => setShowPassword(!showPassword);
 
   const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
